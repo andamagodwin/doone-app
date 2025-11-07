1bb38c614909207873a545ab5ad1a12efc3b556e
andamagodwin <andamagodwinezra@gmail.com>
2025-11-07 22:05:14 +0300

    Refactor Home component to simplify layout and remove unused imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,11 @@
-import { Stack } from 'expo-router';
 
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { View,Text } from 'react-native';
 
 export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
-    </>
+    <View className='flex-1 items-center justify-start bg-white'>
+      <Text>Welcome Home</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
